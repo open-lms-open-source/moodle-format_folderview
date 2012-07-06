6a1b54f6ef15bd6c8c3a45c6f8421ca76f70f984
Mark Nielsen <mark@moodlerooms.com>
2012-07-06 15:40:12 -0700

    INT-3655: Beefing up the version.php

--- version.php
+++ version.php
@@ -7,3 +7,7 @@
  */
 
 $plugin->version  = 2011102000;
+$plugin->requires = 2011120503;
+$plugin->component = 'format_folderview';
+$plugin->release = '2.2.0 (Build: 20120712)';
+$plugin->maturity = MATURITY_STABLE;
