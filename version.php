2856957d692ccbb3368f8371fee9707910edee4f
Mark Nielsen <mark@moodlerooms.com>
2014-06-03 15:25:01 -0700

    INT-6148: Open Source Plug-ins for 2.6
    Bump version information for format_folderview

--- version.php
+++ version.php
@@ -30,8 +30,8 @@
  */
 
 $plugin->version  = 2013021200;
-$plugin->requires = 2013051401;
+$plugin->requires = 2013111803;
 $plugin->component = 'format_folderview';
-$plugin->release = '2.5.0 (Build: 20131227)';
+$plugin->release = '2.6.0 (Build: 20140603)';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array('local_mr' => 2010090201);
