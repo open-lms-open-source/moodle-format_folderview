28c5e1a548d169941f90d5c2e045684a051cbc83
gthomas2 <gthomas@moodlerooms.com>
2016-07-18 14:24:52 +0100

    INT-9961: Update version.php for moodle 3

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version  = 2016012600;
-$plugin->requires = 2015051103;
+$plugin->version   = 2016012600;
+$plugin->requires  = 2015111604;       // Requires this Moodle version (3.0.4).
 $plugin->component = 'format_folderview';
-$plugin->release = '2.9.3 (Build: 20160204)';
-$plugin->maturity = MATURITY_STABLE;
+$plugin->release   = '3.0.4 (Build: 20160509)';
+$plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array('local_mr' => 2016012600);
