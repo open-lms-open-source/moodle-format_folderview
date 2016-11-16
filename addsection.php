e43090cbfcb99b55f756a3fe32e22767c44aadbf
Jonathan Garcia <jonathan.garcia@blackboard.com>
2016-11-16 19:22:37 +0000

    INT-8796: Change in string length function

--- addsection.php
+++ addsection.php
@@ -35,7 +35,7 @@ require_sesskey();
 
 $course = course_get_format($courseid)->get_course();
 
-if (strlen($sectioname) < 256) {
+if (core_text::strlen($sectioname) < 256) {
     $course->numsections++;
     course_create_sections_if_missing($course, range(0, $course->numsections));
     $modinfo = get_fast_modinfo($course);
