9fc8107c132212b3da0bd1b44b4e1db7f54f2480
Mark Nielsen <mark@moodlerooms.com>
2017-09-13 15:15:02 -0700

    INT-12069: Update release/requires for 3.3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017063000;
-$plugin->requires  = 2016120500;
+$plugin->requires  = 2017051500;
 $plugin->component = 'format_folderview';
-$plugin->release   = '3.2.3';
+$plugin->release   = '3.3.1';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = array('local_mr' => 2017063000);
