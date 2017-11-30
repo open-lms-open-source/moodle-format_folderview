58158bf3cd6f699fc2b4a91243f5e0be42dcc73c
Sam Chaffee <sam@moodlerooms.com>
2017-11-30 11:57:22 -0700

    INT-12686: Importing ca lang

---
+++ lang/ca/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2017 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Activitats';
+$string['addblock'] = 'Afegeix un bloc';
+$string['showaddblock'] = 'Mostra Afegeix un bloc';
+$string['hideaddblock'] = 'Amaga Afegeix un bloc';
+$string['addresource'] = 'Afegeix un recurs';
+$string['showaddresource'] = 'Mostra Afegeix un recurs';
+$string['hideaddresource'] = 'Amaga Afegeix un recurs';
+$string['sectionaddresource'] = 'Afegeix un recurs';
+$string['addtopic'] = 'Afegeix un tema';
+$string['showaddtopic'] = 'Mostra Afegeix un tema';
+$string['hideaddtopic'] = 'Amaga Afegeix un tema';
+$string['addtotopic'] = 'Afegeix a';
+$string['close'] = 'Tanca';
+$string['collapseall'] = 'Contreu tots els temes';
+$string['currentsection'] = 'Tema actual';
+$string['deletesection'] = 'Suprimeix el tema';
+$string['expandall'] = 'Expandeix tots els temes';
+$string['sectionexpand'] = 'Expandeix el tema {$a}';
+$string['sectioncollapse'] = 'Contreu el tema {$a}';
+$string['sectionnotavailable'] = '{$a} no és disponible';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Format de la vista de carpetes';
+$string['resources'] = 'Recursos';
+$string['sectionname'] = 'Tema';
+$string['section0name'] = 'Llista de temes';
+$string['sectiontitle'] = 'Nom del tema';
+$string['topicsettings'] = 'Paràmetres del tema';
+$string['viewall'] = 'Mostra\'ls tots';
+$string['hidefromothers'] = 'Oculta el tema';
+$string['showfromothers'] = 'Mostra el tema';
+$string['showalltopics'] = 'Mostra tots els temes';
+$string['showonlytopic'] = 'Mostra només {$a}';
+$string['showonly'] = 'Mostra el tema';
+$string['currenttopic'] = 'Aquest tema';
+$string['deletecoursedone'] = 'Suprimit - Vista de carpetes';
+$string['viewalltopics'] = 'Torna a la pàgina de cursos amb una llista de tots els temes';
+$string['editsection'] = 'Edita el tema';
+$string['mustselectresource'] = 'No s\'ha pogut afegir el recurs perquè no s’ha seleccionat el tipus de recurs. Torneu enrere i seleccioneu el tipus de recurs abans de fer cap tramesa.';
+$string['numberoftopics'] = 'Nombre de temes';
+$string['topicexpanded'] = 'Tema expandit';
+$string['topiccollapsed'] = 'Tema contret';
+$string['alltopicsexpanded'] = 'Tots els temes estan expandits';
+$string['alltopicscollapsed'] = 'Tots els temes estan contrets';
+$string['err_sectionnanme'] = 'El nom del tema no pot superar els 255 caràcters.';
