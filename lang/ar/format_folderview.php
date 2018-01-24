d20bc505be6870fc741f08c5ad678f642861d811
vagrant <vagrant@vagrant.vm>
2018-01-24 16:45:10 +0000

    INT-12838: Importing ar lang

---
+++ lang/ar/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'أنشطة';
+$string['addblock'] = 'إضافة كتلة';
+$string['showaddblock'] = 'إظهار إضافة كتلة';
+$string['hideaddblock'] = 'إخفاء إضافة كتلة';
+$string['addresource'] = 'إضافة مورد';
+$string['showaddresource'] = 'إظهار إضافة مورد';
+$string['hideaddresource'] = 'إخفاء إضافة مورد';
+$string['sectionaddresource'] = 'إضافة مورد';
+$string['addtopic'] = 'إضافة موضوع';
+$string['showaddtopic'] = 'إظهار إضافة موضوع';
+$string['hideaddtopic'] = 'إخفاء إضافة موضوع';
+$string['addtotopic'] = 'إضافة إلى';
+$string['close'] = 'إغلاق';
+$string['collapseall'] = 'طي جميع المواضيع';
+$string['currentsection'] = 'الموضوع الحالي';
+$string['deletesection'] = 'حذف موضوع';
+$string['expandall'] = 'توسيع جميع المواضيع';
+$string['sectionexpand'] = 'توسيع الموضوع {$a}';
+$string['sectioncollapse'] = 'طي الموضوع {$a}';
+$string['sectionnotavailable'] = '{$a} غير متاح';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'تنسيق طريقة عرض المجلدات';
+$string['resources'] = 'الموارد';
+$string['sectionname'] = 'الموضوع';
+$string['section0name'] = 'قائمة المواضيع';
+$string['sectiontitle'] = 'اسم الموضوع';
+$string['topicsettings'] = 'إعدادات الموضوع';
+$string['viewall'] = 'عرض الكل';
+$string['hidefromothers'] = 'إخفاء موضوع';
+$string['showfromothers'] = 'إظهار الموضوع';
+$string['showalltopics'] = 'إظهار جميع المواضيع';
+$string['showonlytopic'] = 'عرض فقط {$a}';
+$string['showonly'] = 'عرض موضوع';
+$string['currenttopic'] = 'هذا الموضوع';
+$string['deletecoursedone'] = 'تم الحذف - طريقة عرض المجلدات';
+$string['viewalltopics'] = 'الرجوع إلى صفحة المقرر الدراسي التي تتضمن قائمة بجميع المواضيع';
+$string['editsection'] = 'تحرير موضوع';
+$string['mustselectresource'] = 'فشل إضافة مورد بسب عدم وجود نوع مورد محدد. يرجى الرجوع وتحديد أحد أنواع الموارد قبل التقديم.';
+$string['numberoftopics'] = 'عدد المواضيع';
+$string['topicexpanded'] = 'تم توسيع موضوع';
+$string['topiccollapsed'] = 'تم طي موضوع';
+$string['alltopicsexpanded'] = 'تم الآن توسيع جميع المواضيع';
+$string['alltopicscollapsed'] = 'تم الآن طي جميع المواضيع';
+$string['err_sectionnanme'] = 'لا يمكن أن يزيد عدد أحرف اسم الموضوع عن 255 حرفًا.';
