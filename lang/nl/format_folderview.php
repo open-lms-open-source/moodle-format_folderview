9ce482db9dcb7c95cf340d44888e6cc624b72969
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:36 +0000

    INT-13192: Importing nl lang

---
+++ lang/nl/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Activiteiten';
+$string['addblock'] = 'Voeg blok toe';
+$string['showaddblock'] = 'Toon Voeg blok toe';
+$string['hideaddblock'] = 'Verberg Voeg blok toe';
+$string['addresource'] = 'Voeg bron toe';
+$string['showaddresource'] = 'Toon Voeg bron toe';
+$string['hideaddresource'] = 'Verberg Voeg bron toe';
+$string['sectionaddresource'] = 'Voeg bron toe';
+$string['addtopic'] = 'Voeg onderwerp toe';
+$string['showaddtopic'] = 'Toon Voeg onderwerp toe';
+$string['hideaddtopic'] = 'Verberg Voeg onderwerp toe';
+$string['addtotopic'] = 'Voeg toe aan';
+$string['close'] = 'Sluit';
+$string['collapseall'] = 'Alle onderwerpen samenvouwen';
+$string['currentsection'] = 'Huidig onderwerp';
+$string['deletesection'] = 'Onderwerp verwijderen';
+$string['expandall'] = 'Alle onderwerpen uitvouwen';
+$string['sectionexpand'] = 'Vouw onderwerp {$a} uit';
+$string['sectioncollapse'] = 'Vouw onderwerp {$a} samen';
+$string['sectionnotavailable'] = '{$a} is niet beschikbaar';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Indeling mapweergave';
+$string['resources'] = 'Bronnen';
+$string['sectionname'] = 'Onderwerp';
+$string['section0name'] = 'Lijst met onderwerpen';
+$string['sectiontitle'] = 'Naam onderwerp';
+$string['topicsettings'] = 'Instellingen onderwerp';
+$string['viewall'] = 'Bekijk alle';
+$string['hidefromothers'] = 'Verberg onderwerp';
+$string['showfromothers'] = 'Toon onderwerp';
+$string['showalltopics'] = 'Toon alle onderwerpen';
+$string['showonlytopic'] = 'Bekijk alleen {$a}';
+$string['showonly'] = 'Bekijk onderwerp';
+$string['currenttopic'] = 'Dit onderwerp';
+$string['deletecoursedone'] = 'Verwijderd - mapweergave';
+$string['viewalltopics'] = 'Ga terug naar de cursuspagina met een lijst van alle onderwerpen';
+$string['editsection'] = 'Bewerk onderwerp';
+$string['mustselectresource'] = 'Kan geen bron toevoegen, omdat  geen brontype is geselecteerd. Ga terug en selecteer een brontype vóór verzending.';
+$string['numberoftopics'] = 'Aantal onderwerpen';
+$string['topicexpanded'] = 'Onderwerp uitgevouwen';
+$string['topiccollapsed'] = 'Onderwerp samengevouwen';
+$string['alltopicsexpanded'] = 'Alle onderwerpen zijn nu uitgevouwen';
+$string['alltopicscollapsed'] = 'Alle onderwerpen zijn nu samengevouwen';
+$string['err_sectionnanme'] = 'Onderwerptitel mag niet langer dan 255 tekens zijn.';
