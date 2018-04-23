8007068c556cab30a844cecca577f2a7caf09a99
David Castro <david.castro@blackboard.com>
2018-04-23 15:33:11 +0000

    INT-13192: Importing de lang

---
+++ lang/de/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Aktivitäten';
+$string['addblock'] = 'Block hinzufügen';
+$string['showaddblock'] = '„Block hinzufügen“ anzeigen';
+$string['hideaddblock'] = '„Block hinzufügen“ verbergen';
+$string['addresource'] = 'Arbeitsmaterialien hinzufügen';
+$string['showaddresource'] = '„Arbeitsmaterialien hinzufügen“ anzeigen';
+$string['hideaddresource'] = '„Arbeitsmaterialien hinzufügen“ verbergen';
+$string['sectionaddresource'] = 'Arbeitsmaterialien hinzufügen';
+$string['addtopic'] = 'Thema hinzufügen';
+$string['showaddtopic'] = '„Thema hinzufügen“ anzeigen';
+$string['hideaddtopic'] = '„Thema hinzufügen“ verbergen';
+$string['addtotopic'] = 'Hinzufügen zu';
+$string['close'] = 'Schließen';
+$string['collapseall'] = 'Alle Themen einklappen';
+$string['currentsection'] = 'Aktuelles Thema';
+$string['deletesection'] = 'Thema löschen';
+$string['expandall'] = 'Alle Themen erweitern';
+$string['sectionexpand'] = 'Thema {$a} erweitern';
+$string['sectioncollapse'] = 'Thema {$a} einklappen';
+$string['sectionnotavailable'] = '{$a} ist nicht verfügbar.';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Format der Ordneransicht';
+$string['resources'] = 'Arbeitsmaterialien';
+$string['sectionname'] = 'Thema';
+$string['section0name'] = 'Themenliste';
+$string['sectiontitle'] = 'Themenname';
+$string['topicsettings'] = 'Themeneinstellungen';
+$string['viewall'] = 'Alle anzeigen';
+$string['hidefromothers'] = 'Thema verbergen';
+$string['showfromothers'] = 'Thema anzeigen';
+$string['showalltopics'] = 'Alle Themen anzeigen';
+$string['showonlytopic'] = 'Nur {$a} anzeigen';
+$string['showonly'] = 'Thema ansehen';
+$string['currenttopic'] = 'Dieses Thema';
+$string['deletecoursedone'] = 'Gelöscht – Ordneransicht';
+$string['viewalltopics'] = 'Zur Kursseite mit einer Übersicht aller Themen zurückkehren';
+$string['editsection'] = 'Thema bearbeiten';
+$string['mustselectresource'] = 'Die Arbeitsmaterialien konnten nicht hinzugefügt werden, da kein Arbeitsmaterialientyp ausgewählt wurde.  Gehen Sie bitte zurück und wählen Sie vor dem Absenden einen Arbeitsmaterialientyp aus.';
+$string['numberoftopics'] = 'Zahl der Themen';
+$string['topicexpanded'] = 'Thema erweitert';
+$string['topiccollapsed'] = 'Thema geschlossen';
+$string['alltopicsexpanded'] = 'Jetzt sind alle Themen erweitert.';
+$string['alltopicscollapsed'] = 'Jetzt sind alle Themen eingeklappt.';
+$string['err_sectionnanme'] = 'Themenname darf nicht mehr als 255 Zeichen enthalten.';
