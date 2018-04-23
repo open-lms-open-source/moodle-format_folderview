859894b77859c0ad3e306edb8018119d032e0e5d
David Castro <david.castro@blackboard.com>
2018-04-23 15:42:57 +0000

    INT-13192: Importing es lang

---
+++ lang/es/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Actividades';
+$string['addblock'] = 'Agregar bloque';
+$string['showaddblock'] = 'Mostrar Agregar Bloque';
+$string['hideaddblock'] = 'Ocultar Agregar Bloque';
+$string['addresource'] = 'Agregar recurso';
+$string['showaddresource'] = 'Mostrar Agregar Recurso';
+$string['hideaddresource'] = 'Ocultar Agregar Recurso';
+$string['sectionaddresource'] = 'Agregar recurso';
+$string['addtopic'] = 'Agregar tema';
+$string['showaddtopic'] = 'Mostrar Agregar Tema';
+$string['hideaddtopic'] = 'Ocultar Agregar Tema';
+$string['addtotopic'] = 'Agregar a';
+$string['close'] = 'Cerrar';
+$string['collapseall'] = 'Colapsar todos los temas';
+$string['currentsection'] = 'Tema actual';
+$string['deletesection'] = 'Eliminar tema';
+$string['expandall'] = 'Expandir todos los temas';
+$string['sectionexpand'] = 'Expandir tema {$a}';
+$string['sectioncollapse'] = 'Colapsar tema {$a}';
+$string['sectionnotavailable'] = '{$a} no está disponible';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Formato de la vista de carpetas';
+$string['resources'] = 'Recursos';
+$string['sectionname'] = 'Tema';
+$string['section0name'] = 'Lista de temas';
+$string['sectiontitle'] = 'Nombre del tema';
+$string['topicsettings'] = 'Ajustes del tema';
+$string['viewall'] = 'Ver todos';
+$string['hidefromothers'] = 'Ocultar tema';
+$string['showfromothers'] = 'Mostrar tema';
+$string['showalltopics'] = 'Mostrar todos los temas';
+$string['showonlytopic'] = 'Ver solo {$a}';
+$string['showonly'] = 'Ver tema';
+$string['currenttopic'] = 'Este tema';
+$string['deletecoursedone'] = 'Eliminado: Vista de carpetas';
+$string['viewalltopics'] = 'Volver a la página del curso con una lista de todos los temas';
+$string['editsection'] = 'Editar tema';
+$string['mustselectresource'] = 'No se pudo agregar el recurso porque no se seleccionó ningún tipo de recurso. Vuelva y seleccione un tipo de recurso antes de enviar.';
+$string['numberoftopics'] = 'Número de temas';
+$string['topicexpanded'] = 'Tema expandido';
+$string['topiccollapsed'] = 'Tema colapsado';
+$string['alltopicsexpanded'] = 'Todos los temas se encuentran expandidos';
+$string['alltopicscollapsed'] = 'Todos los temas se encuentran colapsados';
+$string['err_sectionnanme'] = 'El nombre del tema no puede contener más de 255 caracteres.';
