9b9f32e2a716a973446e63c319a12b7ebbe5408e
David Castro <david.castro@blackboard.com>
2018-04-23 15:30:44 +0000

    INT-13192: Importing ja lang

---
+++ lang/ja/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = '活動';
+$string['addblock'] = 'ブロックを追加する';
+$string['showaddblock'] = '[ブロックを追加する]を表示する';
+$string['hideaddblock'] = '[ブロックを追加する]を隠す';
+$string['addresource'] = 'リソースを追加する';
+$string['showaddresource'] = '[リソースを追加する]を表示する';
+$string['hideaddresource'] = '[リソースを追加する]を隠す';
+$string['sectionaddresource'] = 'リソースを追加する';
+$string['addtopic'] = 'トピックを追加する';
+$string['showaddtopic'] = '[トピックを追加する]を表示する';
+$string['hideaddtopic'] = '[トピックを追加する]を隠す';
+$string['addtotopic'] = '追加先';
+$string['close'] = '閉じる';
+$string['collapseall'] = 'すべてのトピックを折りたたむ';
+$string['currentsection'] = '現在のトピック';
+$string['deletesection'] = 'トピックを削除する';
+$string['expandall'] = 'すべてのトピックを展開する';
+$string['sectionexpand'] = 'トピック {$a} を展開する';
+$string['sectioncollapse'] = 'トピック {$a} を折りたたむ';
+$string['sectionnotavailable'] = '{$a} は利用できません。';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'フォルダビューフォーマット';
+$string['resources'] = 'リソース';
+$string['sectionname'] = 'トピック';
+$string['section0name'] = 'トピック一覧';
+$string['sectiontitle'] = 'トピック名';
+$string['topicsettings'] = 'トピック設定';
+$string['viewall'] = 'すべて表示する';
+$string['hidefromothers'] = 'トピックを隠す';
+$string['showfromothers'] = 'トピックを表示する';
+$string['showalltopics'] = 'すべてのトピックを表示する';
+$string['showonlytopic'] = '{$a} のみを表示する';
+$string['showonly'] = 'トピックを表示する';
+$string['currenttopic'] = 'このトピック';
+$string['deletecoursedone'] = '削除済み - フォルダビュー';
+$string['viewalltopics'] = 'すべてのトピックの一覧があるコースページに戻る';
+$string['editsection'] = 'トピックを編集する';
+$string['mustselectresource'] = 'リソースタイプが選択されていないため、リソースが追加できませんでした。前の操作に戻って、リソースタイプを選択してください。';
+$string['numberoftopics'] = 'トピック数';
+$string['topicexpanded'] = 'トピックは展開されています';
+$string['topiccollapsed'] = 'トピックは折りたたまれています';
+$string['alltopicsexpanded'] = 'すべてのトピックが展開されています';
+$string['alltopicscollapsed'] = 'すべてのトピックが折りたたまれています';
+$string['err_sectionnanme'] = '255文字を超えるトピック名は指定できません。';
