91dc563a5ef7b78f9ae3b72d280ef41a58296604
David Castro <david.castro@blackboard.com>
2018-04-23 15:39:16 +0000

    INT-13192: Importing cs lang

---
+++ lang/cs/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Aktivity';
+$string['addblock'] = 'Přidat blok';
+$string['showaddblock'] = 'Zobrazit okno Přidat blok';
+$string['hideaddblock'] = 'Skrýt okno Přidat blok';
+$string['addresource'] = 'Přidat zdroj';
+$string['showaddresource'] = 'Zobrazit okno Přidat zdroj';
+$string['hideaddresource'] = 'Skrýt okno Přidat zdroj';
+$string['sectionaddresource'] = 'Přidat zdroj';
+$string['addtopic'] = 'Přidat téma';
+$string['showaddtopic'] = 'Zobrazit okno Přidat téma';
+$string['hideaddtopic'] = 'Skrýt okno Přidat téma';
+$string['addtotopic'] = 'Přidat k';
+$string['close'] = 'Zavřít';
+$string['collapseall'] = 'Sbalit všechna témata';
+$string['currentsection'] = 'Aktuální téma';
+$string['deletesection'] = 'Odstranit téma';
+$string['expandall'] = 'Rozbalit všechna témata';
+$string['sectionexpand'] = 'Rozbalit téma {$a}';
+$string['sectioncollapse'] = 'Sbalit téma {$a}';
+$string['sectionnotavailable'] = '{$a} není k dispozici.';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Formát zobrazení složky';
+$string['resources'] = 'Zdroje';
+$string['sectionname'] = 'Téma';
+$string['section0name'] = 'Seznam témat';
+$string['sectiontitle'] = 'Název tématu';
+$string['topicsettings'] = 'Nastavení tématu';
+$string['viewall'] = 'Zobrazit vše';
+$string['hidefromothers'] = 'Skrýt téma';
+$string['showfromothers'] = 'Zobrazit téma';
+$string['showalltopics'] = 'Zobrazit všechna témata';
+$string['showonlytopic'] = 'Zobrazit pouze téma {$a}';
+$string['showonly'] = 'Zobrazit téma';
+$string['currenttopic'] = 'Toto téma';
+$string['deletecoursedone'] = 'Zobrazení složky odstraněných položek';
+$string['viewalltopics'] = 'Návrat na stránku kurzu se seznamem všech témat';
+$string['editsection'] = 'Upravit téma';
+$string['mustselectresource'] = 'Nepodařilo se přidat zdroj, protože nebyl vybrán žádný typ zdroje. Vraťte se prosím zpět a před odesláním vyberte typ zdroje.';
+$string['numberoftopics'] = 'Počet témat';
+$string['topicexpanded'] = 'Téma rozbaleno';
+$string['topiccollapsed'] = 'Téma sbaleno';
+$string['alltopicsexpanded'] = 'Všechna témata jsou rozbalena';
+$string['alltopicscollapsed'] = 'Všechna témata jsou sbalena';
+$string['err_sectionnanme'] = 'Název tématu nemůže mít více než 255 znaků.';
