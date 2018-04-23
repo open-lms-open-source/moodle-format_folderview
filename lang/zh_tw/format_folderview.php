5a8591639e1118f0a50ca8be8e9f780a3cf5796e
David Castro <david.castro@blackboard.com>
2018-04-23 15:43:20 +0000

    INT-13192: Importing zh_tw lang

---
+++ lang/zh_tw/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = '活動';
+$string['addblock'] = '新增區塊';
+$string['showaddblock'] = '顯示新增區塊';
+$string['hideaddblock'] = '隱藏新增區塊';
+$string['addresource'] = '新增資源';
+$string['showaddresource'] = '顯示新增資源';
+$string['hideaddresource'] = '隱藏新增資源';
+$string['sectionaddresource'] = '新增資源';
+$string['addtopic'] = '新增主題';
+$string['showaddtopic'] = '顯示新增主題';
+$string['hideaddtopic'] = '隱藏新增主題';
+$string['addtotopic'] = '新增至';
+$string['close'] = '關閉';
+$string['collapseall'] = '收闔所有主題';
+$string['currentsection'] = '目前的主題';
+$string['deletesection'] = '刪除主題';
+$string['expandall'] = '展開所有主題';
+$string['sectionexpand'] = '展開主題 {$a}';
+$string['sectioncollapse'] = '收闔主題 {$a}';
+$string['sectionnotavailable'] = '無法使用 {$a}';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = '資料夾檢視格式';
+$string['resources'] = '資源';
+$string['sectionname'] = '主題';
+$string['section0name'] = '主題清單';
+$string['sectiontitle'] = '主題名稱';
+$string['topicsettings'] = '主題設定';
+$string['viewall'] = '檢視全部';
+$string['hidefromothers'] = '隱藏主題';
+$string['showfromothers'] = '顯示主題';
+$string['showalltopics'] = '顯示所有主題';
+$string['showonlytopic'] = '僅檢視 {$a}';
+$string['showonly'] = '檢視主題';
+$string['currenttopic'] = '此主題';
+$string['deletecoursedone'] = '已刪除 - 資料夾檢視';
+$string['viewalltopics'] = '回到含所有主題清單的課程頁面';
+$string['editsection'] = '編輯主題';
+$string['mustselectresource'] = '未選取資源類型，因此無法新增資源。請返回並選取資源類型後再送出。';
+$string['numberoftopics'] = '主題數';
+$string['topicexpanded'] = '已展開的主題';
+$string['topiccollapsed'] = '已收闔的主題';
+$string['alltopicsexpanded'] = '現在所有主題都已展開';
+$string['alltopicscollapsed'] = '現在所有主題都已收闔';
+$string['err_sectionnanme'] = '主題名稱不可超過 255 個字元。';
