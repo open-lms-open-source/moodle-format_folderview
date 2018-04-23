fe7a384cf2e2ffeb72ae39bd0e15ca7ae6ca826f
David Castro <david.castro@blackboard.com>
2018-04-23 15:41:05 +0000

    INT-13192: Importing pl lang

---
+++ lang/pl/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Aktywności';
+$string['addblock'] = 'Dodaj blok';
+$string['showaddblock'] = 'Pokaż opcję Dodaj blok';
+$string['hideaddblock'] = 'Ukryj opcję Dodaj blok';
+$string['addresource'] = 'Dodaj zasób';
+$string['showaddresource'] = 'Pokaż opcję Dodaj zasób';
+$string['hideaddresource'] = 'Ukryj opcję Dodaj zasób';
+$string['sectionaddresource'] = 'Dodaj zasób';
+$string['addtopic'] = 'Dodaj temat';
+$string['showaddtopic'] = 'Pokaż opcję Dodaj temat';
+$string['hideaddtopic'] = 'Ukryj opcję Dodaj temat';
+$string['addtotopic'] = 'Dodaj do';
+$string['close'] = 'Zamknij';
+$string['collapseall'] = 'Zwiń wszystkie tematy';
+$string['currentsection'] = 'Bieżący temat';
+$string['deletesection'] = 'Usuń temat';
+$string['expandall'] = 'Rozwiń wszystkie tematy';
+$string['sectionexpand'] = 'Rozwiń temat {$a}';
+$string['sectioncollapse'] = 'Zwiń temat {$a}';
+$string['sectionnotavailable'] = '{$a} jest niedostępna';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Format widoku folderu';
+$string['resources'] = 'Zasoby';
+$string['sectionname'] = 'Temat';
+$string['section0name'] = 'Lista tematów';
+$string['sectiontitle'] = 'Nazwa tematu';
+$string['topicsettings'] = 'Ustawienia tematów';
+$string['viewall'] = 'Wyświetl wszystko';
+$string['hidefromothers'] = 'Ukryj temat';
+$string['showfromothers'] = 'Pokaż temat';
+$string['showalltopics'] = 'Pokaż wszystkie tematy';
+$string['showonlytopic'] = 'Pokaż tylko {$a}';
+$string['showonly'] = 'Wyświetl temat';
+$string['currenttopic'] = 'Ten temat';
+$string['deletecoursedone'] = 'Usunięte — Widok folderu';
+$string['viewalltopics'] = 'Powrót do strony kursy zawierającej listę wszystkich tematów';
+$string['editsection'] = 'Edytuj temat';
+$string['mustselectresource'] = 'Nie można dodać zasobu, ponieważ nie wybrano typu zasobu. Wróć i wybierz typ zasobu przed jego przesłaniem.';
+$string['numberoftopics'] = 'Liczba tematów';
+$string['topicexpanded'] = 'Temat rozwinięty';
+$string['topiccollapsed'] = 'Temat zwinięty';
+$string['alltopicsexpanded'] = 'Wszystkie tematy są teraz rozwinięte';
+$string['alltopicscollapsed'] = 'Wszystkie tematy są teraz zwinięte';
+$string['err_sectionnanme'] = 'Długość nazwy tematu nie może przekraczać 255 znaków.';
