5ad07025549cf715d10d9193b4c1a0a1b2b8221f
David Castro <david.castro@blackboard.com>
2018-06-13 14:04:46 +0000

    INT-13365: Importing tr lang

---
+++ lang/tr/format_folderview.php
@@ -0,0 +1,68 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['activities'] = 'Etkinlikler';
+$string['addblock'] = 'Blok Ekle';
+$string['showaddblock'] = 'Blok Eklemeyi Göster';
+$string['hideaddblock'] = 'Blok Eklemeyi Gizle';
+$string['addresource'] = 'Kaynak Ekle';
+$string['showaddresource'] = 'Kaynak Eklemeyi Göster';
+$string['hideaddresource'] = 'Kaynak Eklemeyi Gizle';
+$string['sectionaddresource'] = 'Kaynak ekle';
+$string['addtopic'] = 'Konu Ekle';
+$string['showaddtopic'] = 'Konu Eklemeyi Göster';
+$string['hideaddtopic'] = 'Konu Eklemeyi Gizle';
+$string['addtotopic'] = 'Buna Ekle:';
+$string['close'] = 'Kapat';
+$string['collapseall'] = 'Tüm konuları daralt';
+$string['currentsection'] = 'Geçerli konu';
+$string['deletesection'] = 'Konuyu sil';
+$string['expandall'] = 'Tüm konuları genişlet';
+$string['sectionexpand'] = '{$a} adlı konuyu genişlet';
+$string['sectioncollapse'] = '{$a} adlı konuyu daralt';
+$string['sectionnotavailable'] = '{$a} kullanılabilir durumda değil';
+$string['itemspercolumn'] = '15';
+$string['numberofcolumns'] = '1';
+$string['pluginname'] = 'Klasör Görünümü biçimi';
+$string['resources'] = 'Kaynaklar';
+$string['sectionname'] = 'Konu';
+$string['section0name'] = 'Konu listesi';
+$string['sectiontitle'] = 'Konu Adı';
+$string['topicsettings'] = 'Konu Ayarları';
+$string['viewall'] = 'Tümünü Görüntüle';
+$string['hidefromothers'] = 'Konuyu gizle';
+$string['showfromothers'] = 'Konuyu göster';
+$string['showalltopics'] = 'Tüm konuları göster';
+$string['showonlytopic'] = 'Yalnızca {$a} öğesini görüntüle';
+$string['showonly'] = 'Konuyu görüntüle';
+$string['currenttopic'] = 'Bu konu';
+$string['deletecoursedone'] = 'Silindi - Klasör Görünümü';
+$string['viewalltopics'] = 'Bütün konuların listesini içeren bir kurs sayfasına dön';
+$string['editsection'] = 'Konuyu düzenle';
+$string['mustselectresource'] = 'Hiçbir kaynak türü seçilmediğinden kaynak eklenemedi. Lütfen geri gidin ve göndermeden önce bir kaynak türü seçin.';
+$string['numberoftopics'] = 'Konu sayısı';
+$string['topicexpanded'] = 'Konu genişletildi';
+$string['topiccollapsed'] = 'Konu daraltıldı';
+$string['alltopicsexpanded'] = 'Tüm konular şimdi genişletildi';
+$string['alltopicscollapsed'] = 'Tüm konular şimdi daraltıldı';
+$string['err_sectionnanme'] = 'Konu adı 255 karakterden uzun olamaz.';
